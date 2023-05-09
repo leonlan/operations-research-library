88fa35dbd08055e0d04e5a69ec03c3ece855600e
l-lan <l.lan@vu.nl>
2023-05-09 17:31:53 +0200

    Refactor ProblemData a bit more

--- src/ProblemData.py
+++ src/ProblemData.py
@@ -12,36 +12,31 @@ class ProblemData:
     def from_file(cls, fname, problem_type):
         data = cls()
 
-        with open(fname, "r") as data:
-            data.jobs = int(data.readline().strip().split()[0])
-            data.machines = int(data.readline().strip().split()[0])
+        with open(fname, "r") as fh:
+            data.jobs = int(fh.readline().strip().split()[0])
+            data.machines = int(fh.readline().strip().split()[0])
 
             if problem_type == "Distributedflowshop":
-                data.factories = int(data.readline().strip().split()[0])
+                data.factories = int(fh.readline().strip().split()[0])
 
             if problem_type == "Tardinessflowshop":
                 data.due_dates = [
-                    int(x) for x in data.readline().strip().split()
+                    int(x) for x in fh.readline().strip().split()
                 ]
 
-            data.processing = [
-                [int(x) for x in data.readline().strip().split()]
-            ]
+            data.processing = [[int(x) for x in fh.readline().strip().split()]]
 
-            for _j in range(data.jobs - 1):
+            for _ in range(data.jobs - 1):
                 data.processing.append(
-                    [int(x) for x in data.readline().strip().split()]
+                    [int(x) for x in fh.readline().strip().split()]
                 )
 
             if problem_type == "Setupflowshop":
-                for i in range(data.machines):
-                    data.setup.append([])
-                    data.setup[i] = [
-                        [int(x) for x in data.readline().strip().split()]
+                for _ in range(data.machines):
+                    rows = [
+                        [int(x) for x in fh.readline().strip().split()]
+                        for _ in range(data.jobs)
                     ]
-                    for _j in range(data.jobs - 1):
-                        data.setup[i].append(
-                            [int(x) for x in data.readline().strip().split()]
-                        )
+                    data.setup.append(rows)
 
         return data
