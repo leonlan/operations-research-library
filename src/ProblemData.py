6f3c620630c21da71aa743ccc47007422aaedb74
l-lan <l.lan@vu.nl>
2023-05-09 19:36:04 +0200

    Type hinting ProblemData

--- src/ProblemData.py
+++ src/ProblemData.py
@@ -1,50 +1,49 @@
+import os
+from typing import List, Optional, Union
+
+
 class ProblemData:
     def __init__(
         self,
-        num_jobs,
-        num_machines,
-        processing,
-        num_factories=0,
-        due_dates=None,
-        setup=None,
+        num_jobs: int,
+        num_machines: int,
+        processing: List[List[int]],
+        num_factories: int = 0,
+        due_dates: Optional[List[int]] = None,
+        setup: Optional[List[List[int]]] = None,
     ):
         self.num_jobs = num_jobs
         self.num_machines = num_machines  # also: stages/units
-        self.num_factories = num_factories
+        self.processing = processing
 
-        self.processing = processing if processing is not None else []
-        self.due_dates = due_dates if due_dates is not None else []
-        self.setup = setup if setup is not None else []
+        self.num_factories = num_factories
+        self.due_dates = due_dates or []
+        self.setup = setup or []
 
     @classmethod
-    def from_file(cls, fname, problem_type):
+    def from_file(cls, fname: Union[str, os.PathLike], problem_type: str):
         data = {}
 
-        def read_int_values(fh):
+        def read_line(fh):
             return [int(x) for x in fh.readline().strip().split()]
 
-        def read_setup(fh, num_machines, num_jobs):
-            return [
-                [read_int_values(fh) for _ in range(num_jobs)]
-                for _ in range(num_machines)
-            ]
-
         with open(fname, "r") as fh:
-            data["num_jobs"] = read_int_values(fh)[0]
-            data["num_machines"] = read_int_values(fh)[0]
+            data["num_jobs"] = read_line(fh)[0]
+            data["num_machines"] = read_line(fh)[0]
+            data["processing"] = [
+                read_line(fh) for _ in range(data["num_jobs"])
+            ]
 
             if problem_type == "Distributedflowshop":
-                data["num_factories"] = read_int_values(fh)[0]
+                data["num_factories"] = read_line(fh)[0]
 
             if problem_type == "Tardinessflowshop":
-                data["due_dates"] = read_int_values(fh)
-
-            data["processing"] = [
-                read_int_values(fh) for _ in range(data["num_jobs"])
-            ]
+                data["due_dates"] = read_line(fh)
 
             if problem_type == "Setupflowshop":
-                setup = read_setup(fh, data["num_machines"], data["num_jobs"])
-                data["setup"] = setup
+                data["setup"] = [
+                    [read_line(fh) for _ in range(data["num_jobs"])]
+                    for _ in range(data["num_machines"])
+                ]
 
         return cls(**data)
