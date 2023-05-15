944b772270cd81baaf3eec792260ae0afc1f986a
l-lan <l.lan@vu.nl>
2023-05-15 14:27:47 +0200

    Add plotting functionality for HFS

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ from typing import Tuple, Union
 
 import cplex
 import gurobipy as grb
+import matplotlib.pyplot as plt
 import numpy as np
 from cp import CP_MODELS
 from mip.modelCplexMIP import MIPmodel_generation
@@ -181,6 +182,10 @@ def CPLEX_CP_solve(
         schedule = result2schedule(data, result)
         plot_schedule(data, schedule)
 
+    if problem_type == "Hybridflowshop":
+        hfsresult2plot(data, result)
+        plt.show()
+
     return lb, ub
 
 
@@ -200,3 +205,75 @@ def result2schedule(data, result):
         schedule.append(sequence)
 
     return schedule
+
+
+def hfsresult2schedules(data, result):
+    """
+    Extract the schedule for each stage in the HFS.
+    """
+    schedules = []
+
+    for stage in range(data.num_stages):
+        schedule = []
+
+        for machine in range(data.machines[stage]):
+            job_start = []
+
+            for job in range(data.num_jobs):
+                var = result.get_var_solution(f"A_{job}_{stage}_{machine}")
+
+                if not var.is_absent():
+                    job_start.append((job, var.get_start()))
+
+            sequence = [
+                job for (job, *_) in sorted(job_start, key=lambda x: x[1])
+            ]
+            schedule.append(sequence)
+
+        schedules.append(schedule)
+
+    return schedules
+
+
+def hfsresult2plot(data, result):
+    """
+    Extract the schedule for each stage in the HFS.
+    """
+    schedule = []
+
+    for stage in range(data.num_stages):
+        for machine in range(data.machines[stage]):
+            for job in range(data.num_jobs):
+                var = result.get_var_solution(f"A_{job}_{stage}_{machine}")
+                if var.is_absent():
+                    continue
+
+                item = (job, stage, machine, var.get_start(), var.get_length())
+                schedule.append(item)
+
+    plot(data, schedule)
+
+
+def plot(data, schedule, ax=None):
+    _, ax = plt.subplots(data.num_stages, 1, figsize=(10, 10))
+
+    def get_completion_time(item):
+        return item[3] + item[4]
+
+    # Defining custom 'xlim' and 'ylim' values.
+    latest = max(schedule, key=get_completion_time)
+    custom_xlim = (0, get_completion_time(latest))
+
+    # Setting the values for all axes.
+    plt.setp(ax, xlim=custom_xlim)
+
+    # Color per job
+    hsv = plt.get_cmap("hsv")
+    colors = hsv(np.linspace(0, 1.0, data.num_jobs))
+
+    for job, stage, machine, start, duration in schedule:
+        ax[stage].barh(machine, width=duration, left=start, color=colors[job])
+        ax[stage].text(start + duration / 4, machine, job, va="center")
+
+    plt.xlabel("Time")
+    plt.ylabel("Machine")
