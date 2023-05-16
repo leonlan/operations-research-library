5184c6337cc2a8906adec051d0440cf652a78b97
l-lan <l.lan@vu.nl>
2023-05-16 16:05:16 +0200

    Remove model type, solver and out dir arguments

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Optional
 
 import models
 import typer
@@ -11,28 +10,14 @@ def main(
     problem_type: ProblemType = typer.Option(
         ProblemType.PARALLEL_MACHINE, case_sensitive=False
     ),
-    model_type: str = "cp",
     time_limit: int = 3,
-    solver: str = "cplex",
     num_procs: int = 1,
-    out_dir: Optional[str] = "tmp/",
 ):
     for where in datas:
         path = Path(where)
         num_jobs, num_stages, time, lb, ub, gap = models.main(
-            path,
-            time_limit,
-            problem_type.value,
-            model_type.lower(),
-            solver.lower(),
-            num_procs,
-            out_dir,
+            path, time_limit, problem_type.value, num_procs
         )
-
-        x = f"{out_dir}/{path.stem}.txt"
-        with open(x, "w") as fh:
-            result = f"{problem_type}\t{solver}\t{model_type}\t{path.stem}\t{num_jobs}\t{num_stages}\t{lb}\t{ub}\t{gap}\t{time}"
-            fh.write(result)
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -1,14 +1,10 @@
 import time
 from itertools import product
-from pathlib import Path
 from typing import NamedTuple, Tuple, Union
 
-import cplex
-import gurobipy as grb
 import matplotlib.pyplot as plt
 import numpy as np
 from cp import CP_MODELS
-from mip.modelCplexMIP import MIPmodel_generation
 from plotting import plot_schedule
 from ProblemData import ProblemData
 
@@ -17,10 +13,7 @@ def main(
     path,
     time_limit,
     problem_type,
-    model_type,
-    solver,
     num_procs,
-    out_dir,
 ):
     data = ProblemData.from_file(path, problem_type)
     name = path.stem
@@ -30,31 +23,12 @@ def main(
         "name": name,
         "time_limit": time_limit,
         "num_procs": num_procs,
-        "out_dir": out_dir,
     }
 
     time_start = time.perf_counter()
 
-    if model_type == "mip":
-        if solver == "cplex":
-            model = cplex.Cplex()
-            model = MIPmodel_generation(data, model, problem_type)
-            x, y = CPLEX_MIP_solve(model, **kwargs)
-        elif solver == "gurobi":
-            model = cplex.Cplex()
-            model = MIPmodel_generation(data, model, problem_type)
-            model.write("model.lp")
-            model = grb.read("model.lp")
-
-            x, y = Gurobi_solve(model, **kwargs)
-        else:
-            raise ValueError(f"Unknown solver: {solver}")
-
-    if model_type == "cp":
-        model = CP_MODELS[problem_type](data)
-        x, y = CPLEX_CP_solve(model, data=data, **kwargs)
-    else:
-        raise ValueError(f"Unknown model type: {model_type}")
+    model = CP_MODELS[problem_type](data)
+    x, y = CPLEX_CP_solve(model, data=data, **kwargs)
 
     elapsed_time = round(time.perf_counter() - time_start, 3)
 
@@ -66,83 +40,11 @@ def main(
     return (data.num_jobs, data.num_machines, elapsed_time, x, y, gap_or_msg)
 
 
-def Gurobi_solve(model, problem_type, name, time_limit, num_procs, out_dir):
-    model.setParam("MIPGapAbs", 0.99999)
-    model.setParam("MIPGap", 0.0000)
-    model.setParam("Timelimit", time_limit)
-    model.setParam("Threads", num_procs)
-
-    model.optimize()
-
-    if model.status != 1:
-        fname = "solution_MIP_Gurobi_{}_{}.txt".format(problem_type, name)
-        with open(Path(out_dir) / fname, "w") as fh:
-            fh.write(
-                "\n MIP Gurobi {} - {} - ({} - {})".format(
-                    problem_type,
-                    name,
-                    int(np.round(model.objbound)),
-                    int(np.round(model.objVal)),
-                )
-            )
-            for v in model.getVars():
-                if v.x != 0:
-                    if v.varName.startswith("C"):
-                        fh.write("\n")
-                        fh.write(v.varName)
-                        fh.write(" ")
-                        fh.write(str(np.round(v.x)))
-        return int(np.round(model.objbound)), int(np.round(model.objVal))
-    else:
-        return "Infeasible", "Unkown"
-
-
-def CPLEX_MIP_solve(model, problem_type, name, time_limit, num_procs, out_dir):
-    model.parameters.timelimit.set(time_limit)
-    model.parameters.mip.tolerances.absmipgap.set(0.99999)
-    model.parameters.mip.tolerances.mipgap.set(0.00000)
-    model.parameters.threads.set(num_procs)
-    model.set_log_stream(None)
-    model.set_error_stream(None)
-    model.set_warning_stream(None)
-    model.set_results_stream(None)
-    model.solve()
-    if (
-        model.solution.get_status_string(status_code=None)
-        != "integer infeasible"
-        and model.solution.get_status_string(status_code=None)
-        != "time limit exceeded, no integer solution"
-    ):
-        fname = "solution_MIP_CPLEX_{}_{}.txt".format(problem_type, name)
-        with open(Path(out_dir) / fname, "w") as fh:
-            fh.write(
-                "\n MIP CPLEX {} - {} - ({} - {})".format(
-                    problem_type,
-                    name,
-                    int(np.round(model.solution.MIP.get_best_objective())),
-                    int(np.round(model.solution.get_objective_value())),
-                )
-            )
-
-            for i, x in enumerate(model.solution.get_values()):
-                if x != 0:
-                    if model.variables.get_names(i).startswith("C"):
-                        fh.write("\n")
-                        fh.write(model.variables.get_names(i))
-                        fh.write(" ")
-                        fh.write(str(np.round(x)))
-        return int(np.round(model.solution.MIP.get_best_objective())), int(
-            np.round(model.solution.get_objective_value())
-        )
-    else:
-        return "Infeasible", "Unkown"
-
-
 Outcome = Union[int, str]
 
 
 def CPLEX_CP_solve(
-    model, problem_type, name, time_limit, num_procs, out_dir, data
+    model, problem_type, name, time_limit, num_procs, data
 ) -> Tuple[Outcome, Outcome]:
     result = model.solve(
         TimeLimit=time_limit,
@@ -157,27 +59,6 @@ def CPLEX_CP_solve(
 
     lb = result.get_objective_bounds()[0]
     ub = result.get_objective_values()[0]
-
-    fname = f"solution_CP_CPLEX_{problem_type}_{name}.txt"
-
-    with open(Path(out_dir) / fname, "w") as fh:
-        fh.write(f"\n CP {problem_type} - {name} - ({lb} - {ub})")
-
-        for j in range(data.num_jobs):
-            fh.write("\n")
-            num_machines = data.num_machines
-
-            for i in range(num_machines):
-                if problem_type == "Parallelmachine":
-                    var_name = "A_{}_{}".format(j, i)
-                elif problem_type == "Unrelatedparallelmachines":
-                    var_name = "T_{}".format(j)
-                else:
-                    var_name = "T_{}_{}".format(j, i)
-
-                var = result.get_var_solution(var_name)
-                x, y = var.get_end(), var.get_start()
-                fh.write(f"{y} {x}\t")
 
     if problem_type == "Parallelmachine":
         schedule = result2schedule(data, result)
