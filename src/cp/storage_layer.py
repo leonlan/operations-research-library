0c331514343bd3339834d9e5e32fe0adbc09e048
l-lan <l.lan@vu.nl>
2023-05-29 22:49:22 +0200

    Add storage layer model

--- src/cp/storage_layer.py
+++ src/cp/storage_layer.py
@@ -5,27 +5,7 @@ import docplex.cp.model as docp
 
 def storage_layer(data):
     """
-    Create a storage layer model.
-
-    A storage layer is a stage in which jobs do not need to be processed, i.e.,
-    they do not have any processing times. Instead, the intervals are
-    constrained by intra-stage constraints.
-
-    Moreover, storage layers (in the application in compound feed production)
-    must take into account multi-level product characteristics to avoid
-    contamination of goods.
-
-    The machine layout conists of a set of production lines, each of whch
-    consists of one unrelated machine (i.e., storage) unit.
-
-    This model uses the following variables:
-    - $T_j$: interval variable for batch j
-    - $Tp_j$: interval variable for product p
-    - $Ap_{ji}$: interval assignment variable for product p on machine i
-    - Sp_i$: sequence variable of product assignment variables on machine i
-
-    We do not need to take into account batch assignment variables, since
-    that is covered by the product assignment variables.
+    Creates a storage layer CP model.
     """
     model = docp.CpoModel()
 
