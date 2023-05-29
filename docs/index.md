3f3001acb32b77a5a0b0794f059d4b8738198db9
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-05-29 22:36:16 +0200

    Documentation (#20)

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to CP Scheduling
+
+This repository contains numerous prototype models for scheduling with constraint programming.
--- src/cp/distributed_flow_shop.py
+++ src/cp/distributed_flow_shop.py
@@ -7,13 +7,15 @@ from src.ProblemData import ProblemData
 
 def distributed_flow_shop(data: ProblemData) -> docp.CpoModel:
     """
-    Creates a CP model for the distributed flow shop problem.
+    Creates a CP model for the distributed flow shop problem (DFSP).
     """
     model = docp.CpoModel()
 
     tasks = create_tasks_variables(data, model)
     assign = create_assignment_variables(data, model)
     sequences = create_sequence_variables(data, model, assign)
+
+    minimize_makespan(data, model, tasks)
 
     assign_one_line(data, model, assign, tasks)
     assign_only_eligible_lines(data, model, assign)
@@ -23,14 +25,13 @@ def distributed_flow_shop(data: ProblemData) -> docp.CpoModel:
     no_overlap_on_unit(data, model, sequences)
     same_sequence_on_each_unit(data, model, sequences)
 
-    minimize_makespan(data, model, tasks)
-
     return model
 
 
 def create_tasks_variables(data, model):
     """
-    Creates task interval variables for each job and machine combination.
+    Creates a task interval variable $T_{ju}$ for each job $j \\in J$ and
+    unit $u \\in U$.
     """
     tasks = {}
 
@@ -42,8 +43,8 @@ def create_tasks_variables(data, model):
 
 def create_assignment_variables(data, model):
     """
-    Creates an assignment interval variable for each job, machine and line
-    combination.
+    Creates an assignment interval variable $A_{jul}$ for each job $j \\in J$,
+    unit $u \\in U$, and line $l \\in L$.
     """
     assign = {}
 
@@ -58,7 +59,8 @@ def create_assignment_variables(data, model):
 
 def create_sequence_variables(data, model, assign):
     """
-    Creates a sequence variable for each machine and line combination.
+    Creates a sequence variable $S_{ul}$ for each unit $u \\in U$ and
+    line $l \\in L$.
     """
     sequences = {}
 
@@ -70,13 +72,31 @@ def create_sequence_variables(data, model, assign):
     return sequences
 
 
+def minimize_makespan(data, model, tasks):
+    """
+    Minimizes the makespan of the schedule. It implements the objective
+    function:
+
+    \\begin{equation}
+        \\min \\max_{j \\in J} \\texttt{EndOf}(T_{j|M|}).
+    \\end{equation}
+    """
+    last = data.num_units
+    completion_times = [model.end_of(tasks[(j, last - 1)]) for j in data.jobs]
+    makespan = model.max(completion_times)
+    model.add(model.minimize(makespan))
+
+
 def no_overlap_between_units(data, model, tasks):
     """
-    Adds a no overlap constraint for each job and machine combination, which
-    ensures that a job can start on machine $i$ only when it's completed on
-    machine $i-1$.
+    Adds a no-overlap constraint for each job and unit combination, which
+    ensures that a job can start on unit $u$ only when it's completed on
+    unit $u-1$.
 
-        NoOverlap(Tasks[j][i-1], Tasks[j][i])
+    \\begin{equation}
+        \\texttt{NoOverlap}(T_{j, i-1}, T_{ju}),
+        \\quad \\forall j \\in J, u \\in U \\setminus {1}.
+    \\end{equation}
     """
     for j, u in product(data.jobs, range(1, data.num_units)):
         cons = model.end_before_start(tasks[(j, u - 1)], tasks[(j, u)])
@@ -85,12 +105,13 @@ def no_overlap_between_units(data, model, tasks):
 
 def same_sequence_on_each_unit(data, model, sequences):
     """
-    Ensures that the sequence of jobs on each machine is the same for each
-    line. It implements the constraint
+    Ensures that the sequence of jobs is the same for all units of the same
+    production line.
 
-        SameSequence(SeqVar[i][k], SeqVar[i+1][k])
-
-    for each machine $i$ and line $k$ combination.
+    \\begin{equation}
+        \\texttt{SameSequence}(S_{ul}, S_{u+1,l}),
+        \\quad \\forall u \\in U, l \\in L.
+    \\end{equation}
     """
     for u, l in product(range(data.num_units - 1), data.lines):
         cons = model.same_sequence(sequences[(u, l)], sequences[(u + 1, l)])
@@ -100,6 +121,11 @@ def same_sequence_on_each_unit(data, model, sequences):
 def assign_only_eligible_lines(data, model, assign):
     """
     Implements the line eligibility constraints on assignment variables.
+
+    \\begin{equation}
+        \\texttt{PresenceOf}(A_{jul}) = 0,
+        \\quad \\forall u \\in U, l \\in L, j \\in E_{ul}.
+    \\end{equation}
     """
     for job, unit, line in product(data.jobs, data.units, data.lines):
         if not data.eligible[job][unit][line]:
@@ -109,13 +135,16 @@ def assign_only_eligible_lines(data, model, assign):
 
 def no_overlap_on_unit(data, model, seq_var):
     """
-    Ensures that no two jobs are scheduled on the same machine at the same
+    Ensures that no two jobs are scheduled on the same unit at the same
     time. It implements the constraint
 
-        NoOverlap(SeqVar[i][k], setup)
+    \\begin{equation}
+        \\texttt{NoOverlap}(S_{ul}, sdst_{ul}),
+        \\quad \\forall u \\in U, l \\in L,
+    \\end{equation}
 
-    for each machine $i$ and line $k$ combination, where `setup` is a J-by-J
-    matrix of setup times between jobs.
+    where $sdst_{ul}$ is a $J$-by-$J$ matrix of sequence-dependent setup times
+    on unit $u$ and line $l$.
     """
 
     for u, l in product(data.units, data.lines):
@@ -128,9 +157,10 @@ def assign_one_line(data, model, assign, tasks):
     Ensures that each job is assigned to exactly one line. It implements the
     constraint
 
-        Alternative(Tasks[j][i], [Tasks[j][i][k] for k in range(num_lines)])
-
-    for each job $j$ and machine $i$ combination.
+    \\begin{equation}
+        \\texttt{Alternative}(T_{ju}, \\{A_{jul} : l \\in L \\})
+        \\quad \\forall j \\in J.
+    \\end{equation}
     """
     for j, u in product(data.jobs, data.units):
         assign_vars = [assign[(j, u, k)] for k in data.lines]
@@ -142,18 +172,12 @@ def assign_to_all_units_of_single_line(data, model, assign):
     Ensures that if a job is assigned to a line, then all its units are
     scheduled. It implements the constraint
 
-        PresenceOf(Tasks[j][i][k]) >= PresenceOf(Tasks[j][0][k])
-
-    for each job $j$, machine $i$ and line $k$ combination.
+    \\begin{equation}
+        \\texttt{PresenceOf}(A_{jul}) \\geq \\texttt{PresenceOf}(A_{j0l})
+        \\quad \\forall j \\in J, u \\in U \\setminus {1}, l \\in L.
+    \\end{equation}
     """
     for j, u, l in product(data.jobs, range(1, data.num_units), data.lines):
         other = model.presence_of(assign[(j, u, l)])
         first = model.presence_of(assign[(j, 0, l)])
         model.add(other >= first)
-
-
-def minimize_makespan(data, model, tasks):
-    last = data.num_units
-    completion_times = [model.end_of(tasks[(j, last - 1)]) for j in data.jobs]
-    makespan = model.max(completion_times)
-    model.add(model.minimize(makespan))
